a84af75a74102be1a70e25f657e4cb1863cefe40
Dave Sherlock <dsherlock@sentient.ie>
2021-03-09 16:46:16 +0000

    tidy up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,20 @@ import React from "react"
 
 function Footer() {
     return (<footer className="s-footer h-dark-bg">
-
         <div className="right-vert-line"></div>
-
         <div className="row s-footer__main">
             <div className="column large-6">
                 <div className="section-intro" data-aos="fade-up">
                     <h3 className="subhead">Get In Touch</h3>
-
                     <h1 className="display-1">
                         Have an idea or a concept that really excite you?
                         Let's turn that idea into reality. Shoot me a message.
                 </h1>
                 </div>
-
                 <div className="footer-email-us">
                     <a href="mailto:#0" className="btn btn--primary h-full-width">Let's Talk</a>
                 </div>
             </div>
-
             <div className="column large-5">
                 <div className="footer-contacts">
                     <div className="footer-contact-block" data-aos="fade-up">
@@ -54,20 +49,13 @@ function Footer() {
                 </div>
             </div>
         </div>
-
         <div className="row s-footer__bottom">
-            <div className="column large-full ss-copyright">
-                <span>© Copyright Ethos 2019</span>
-                <span>Design by <a href="https://www.styleshout.com/">StyleShout</a></span>
-            </div>
-
             <div className="ss-go-top">
                 <a className="smoothscroll" title="Back to Top" href="#top">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0l8 9h-6v15h-4v-15h-6z" /></svg>
                 </a>
             </div>
         </div>
-
     </footer>)
 }
 
