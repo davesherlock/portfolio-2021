5d9c1a9367f890456ad3c26448d2b1314c578afe
Dave Sherlock <dsherlock@sentient.ie>
2021-03-13 22:46:22 +0000

    removing smooth scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,10 @@ function Header() {
             <div className="header-content">
                 <nav className="row header-nav-wrap">
                     <ul className="header-nav">
-                        <li><a className="smoothscroll" href="#hero" title="Intro">Home</a></li>
-                        <li><a className="smoothscroll" href="#about" title="About">About</a></li>
-                        <li><a className="smoothscroll" href="#portfolio" title="Portfolio">Portfolio</a></li>
-                        <li><a className="smoothscroll" href="#footer" title="Contact us">Say Hello</a></li>
+                        <li><a className="" href="#hero" title="Intro">Home</a></li>
+                        <li><a className="" href="#about" title="About">About</a></li>
+                        <li><a className="" href="#portfolio" title="Portfolio">Portfolio</a></li>
+                        <li><a className="" href="#footer" title="Contact us">Say Hello</a></li>
                     </ul>
                 </nav>
                 <a href="files/DaveSherlock_CV_2021.pdf" className="btn btn--stroke btn--small">Download CV</a>
