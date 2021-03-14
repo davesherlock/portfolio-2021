22c7bc50c18575dd4ee4a32e78d799b1b8d15bd8
Dave Sherlock <dsherlock@sentient.ie>
2021-03-14 14:12:26 +0000

    Sorting out the portfolio image on mobile

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,30 +7,13 @@ function Portfolio() {
                 <div className="column large-6 medium-8 tab-full">
                     <div className="section-intro" data-num="03" data-aos="fade-up">
                         <h3 className="subhead">Portfolio</h3>
-                        <h1 className="display-1">
-                            Want to see what I've been up to?
-                    </h1>
+                        <h1 className="display-1">Want to see what I've been up to?</h1>
                         <br /><br />
                         <p>I don't just show anyone my work. Please request access using the button below. In hac habitasse platea dictumst. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Fusce risus nisl, viverra et, tempor et, pretium in, sapien. Phasellus volutpat, metus eget.</p>
-
                     </div>
-
                     <a href="files/DaveSherlock_CV_2021.pdf" className="btn btn--stroke btn--small">Request Access</a>
-
-
                 </div>
-
-
-                <div className="column large-6 medium-8 tab-full">
-                    <div className="portfolio-pic" data-aos="fade-up">
-                        <img src="images/scorebuddy.png"
-                            alt="Scorebuddy Screen Shot" />
-                    </div>
-                </div>
-
             </div>
-
-
         </section>
     )
 }
