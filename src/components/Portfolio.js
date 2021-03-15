a78cd44c60d41bd8e6efea2bca5acf15b429cfe8
Dave Sherlock <dsherlock@sentient.ie>
2021-03-15 21:04:33 +0000

    fine-tuning

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,12 +7,12 @@ function Portfolio() {
                 <div className="column large-6 medium-8 tab-full">
                     <div className="section-intro" data-num="03" data-aos="fade-up">
                         <h3 className="subhead">Portfolio</h3>
-                        <h1 className="display-1">Want to see what I've been up to?</h1>
+                        <h1 className="display-1">Want to see what <br />I've been up to?</h1>
                         <br />
                         <div className=" mobile-portfolio-pic">
                             <img src="images/scorebuddy-laptop.png" alt="scorebuddy-on-laptop" />
                         </div>
-                        <p>The past decade has been a busy one, with lots of development and user interface design. If you're interested in seeing some of my work, please let me know via the link below.</p>
+                        <p>The past decade has been a busy one, with lots of development and application development. If you're interested in seeing some of my work, please let me know via the link below.</p>
                     </div>
                     <a href="mailto:hello@davesherlock.com" className="btn btn--stroke btn--small">Request Access</a>
                 </div>
