ddc531111da8030a7cfa250bfe50abd5cb21d8cf
Dave Sherlock <dsherlock@sentient.ie>
2021-03-17 20:28:35 +0000

    small tweaks

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,8 +8,7 @@ function Portfolio() {
                     <div className="section-intro" data-num="03" data-aos="fade-up">
                         <h3 className="subhead">Portfolio</h3>
                         <h1 className="display-1">Want to see what <br />I've been up to?</h1>
-                        <br />
-                        <div className=" mobile-portfolio-pic">
+                        <div className="mobile-portfolio-pic">
                             <img src="images/scorebuddy-laptop.png" alt="scorebuddy-on-laptop" />
                         </div>
                         <p>The past decade has been a busy one, with lots of user interface design and application development. If you're interested in seeing some of my work, please let me know via the link below.</p>
